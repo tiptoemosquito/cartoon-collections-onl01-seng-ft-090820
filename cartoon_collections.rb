aa73a09ba7e4cec84d5f3a27880025a27c558867
Christina Howard <clhoward33@icloud.com>
2020-09-13 16:55:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  i = 0
+  while i < dwarves.length 
+    i = i + 1
+  end# Your code here
 end
 
 def summon_captain_planet# code an argument here
